cfc8eb10aa8d8fdea7fc8ae8ef7a868e933524e6
franco2199 <71840388+franco2199@users.noreply.github.com>
2020-11-19 13:49:42 -0500

    confirmar

--- TrabajosDW2020/Sesion 22/js/misScripts.js
+++ TrabajosDW2020/Sesion 22/js/misScripts.js
@@ -1,17 +1,17 @@
 function guardarDatos(){
-   localStorage.nombre = document.getElementById("nombre").Value;
-   localStorage.password = document.getElementById("password").Value;
+    localStorage.nombre =document.getElementById("nombre").value;
+    localStorage.password =document.getElementById("password").value;
 }
 function recuperarDatos(){
     if((localStorage.nombre != undefined) && (localStorage.password != undefined)){
        document.getElementById("datos").innerHTML="Nombre:"+ localStorage.nombre + " <br> Password:" + localStorage.password;
     }else {
-        document.getElementById("Datos").innerHTML ="No has ingresado tu nombre y password";
+        document.getElementById("Datos").innerHTML ="No has ingresado tu nombre y password"
     }
 }
 function borrarDatos(){
     localStorage.clear();
-    document.getElementById("datos").innerHTML = "Ingresa Nombre y Password";
-    document.getElementById("nombre").Value = "";
-    document.getElementById("password").Value = "";
+    document.getElementById("Datos").innerHTML="Ingrese Nombre y Password";
+    document.getElementById("nombre").value ="";
+    document.getElementById("password").value ="";
 }
